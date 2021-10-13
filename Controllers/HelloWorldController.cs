2fa176a648929b935f36ec0457d5647470108131
Eleanor Miller <eleanormiller.dev@gmail.com>
2021-10-13 17:01:42 -0400

    Adds first simple Hello World Controller

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        // The name of the method "SayHello" is to be clear to 
+        // *me* the programmer. It doesn't tell ASP anything.
+        [HttpGet]
+        public string SayHello()
+        {
+            return $"Hello, World. {DateTime.Now}";
+        }
+    }
+}
